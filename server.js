5f357feeaa8652509248462eb056889a7d7918b0
teachergg <jwhitney@bisd.us>
2022-04-24 05:18:19 -0500

    finished app gets/posts and added template data

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
+// Express Boilerplate
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes')
-const htmlRoutes = require('./routes/htmlRoutes')
+
+const { notes } = require('./db/db');
+const uuid = require('uniqid');
 
 app.use(express.urlencoded({ extended:  true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use(apiRoutes);
-app.use('/', htmlRoutes);
+// GET API
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+// POST New Note to db
+app.post('/api/notes', (req, res) => {
+  const existingNotes = JSON.parse(fs.readFileSync('./db/db.json'));
+
+  req.body.id = existingNotes.length.uuid;
+
+  const newNote = req.body;
+  existingNotes.push(newNote);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(existingNotes));
+
+  res.json(existingNotes);
+});
+
+
+// Page Load
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`API Server now on port ${PORT}!`);
